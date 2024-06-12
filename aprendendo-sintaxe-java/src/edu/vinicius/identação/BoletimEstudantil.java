6bb9aea806f7c54dae2e06b2f4bf02e61a4506c0
Vinicius Mateos <vinimateos.s@gmail.com>
2024-06-11 22:33:27 -0300

    feat: Aula de anatomia de classes

---
+++ aprendendo-sintaxe-java/src/edu/vinicius/identação/BoletimEstudantil.java
@@ -0,0 +1,19 @@
+package edu.vinicius.identação;
+
+//IDENTAÇÃO
+
+        //Escrever o código do programa de forma hierárquica para facilitar a legibilidae do código.
+
+//SEM IDENTAÇÃO
+public class BoletimEstudantil {
+public static void main(String[] args) {
+int mediaFinal = 6;
+if(mediaFinal<6)	
+System.out.println("REPROVADO"); 
+else if(mediaFinal==6)
+System.out.println("PROVA MINERVA"); 
+else
+System.out.println("APROVADO"); 		
+}
+}
+    
---
+++ aprendendo-sintaxe-java/src/edu/vinicius/variaveis_metodos/VariaveisMetodos.java
@@ -0,0 +1,42 @@
+package edu.vinicius.variaveis_metodos;
+
+public class VariaveisMetodos {
+    /*DECLARANDO VARIÁVEIS E MÉTODOS
+
+        #################### 
+            ESTRUTURA VARIÁVEIS
+                Tipo NomeBemDefinido = Atribuicao (opcional em alguns casos)
+
+            EXEMPLO
+                int idade = 23;
+            double altura = 1.62;
+            Dog spike; //observe que aqui a variável spike não tem valor, é normal
+        ####################
+
+        ####################
+            ESTRUTURA MÉTODO
+                TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+
+            EXEMPLO
+                //int somar (int numeroUm, int numero2)
+                //String formatarCep (long cep) //Métodos podem tem retorno com valor diferente dos parâmetros, como nesse ex.
+        ####################
+    */
+    public static void main (String [] args){
+        
+        String primeiroNome = "Vinicius";
+        String segundoNome = "Mateos";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome); //Para diferenciar método de variável, método sempre passará com os parâmetros (entre "()")
+        System.out.print(nomeCompleto);
+    }
+
+        public static String nomeCompleto (String primeiroNome, String segundoNome){
+            return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome); //"return" palavra reservada para retornar um valor do método
+        }
+    
+} 
+        
+
+    
+
