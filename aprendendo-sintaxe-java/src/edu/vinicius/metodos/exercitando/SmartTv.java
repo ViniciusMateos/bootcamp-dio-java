e9208773105c100ebcc4fb93ff6f0cd43722cdf4
Vinicius Mateos <vinimateos.s@gmail.com>
2024-06-18 19:15:27 -0300

    feat: aula de métodos com exercicio de SmartTV

---
+++ aprendendo-sintaxe-java/src/edu/vinicius/metodos/exercitando/SmartTv.java
@@ -0,0 +1,58 @@
+package edu.vinicius.metodos.exercitando;
+
+/*
+    EXERCITANDO
+
+        Vamos criar um exemplo de uma classe para representar uma SmartTV onde:
+
+        1 - Ela tenha as características: ligada (boolean), canal (int) e volume (int);
+
+        2 - Nossa TV poderá ligar e desligar e assim mudar o estado ligada;
+
+        3- Nossa TV aumentará e diminuirá o volume sempre em +1 ou -1;
+
+        4 - Nossa TV poderá mudar de canal de 1 em 1 ou definindo o número correspondente.
+*/
+
+public class SmartTv {
+
+    //Passar os atributos que serão necessários para nossa SmartTV
+    // 1
+    boolean ligada=false;
+    int volume = 25;
+    int canal = 1;
+    
+
+    // DEFINIR AS FUNCIONALIDADES ATRAVÉS DE MÉTODOS
+
+    // 2
+    public void ligarTv(){
+        ligada = true;
+    }
+    public void desligarTv(){
+        ligada = false;
+    }
+    //////////////////
+
+    // 3
+    public void aumentarVolume(){
+        volume++;
+    }
+    public void diminuirVolume(){
+        volume--;
+    }
+    ////////////////////
+
+    //4
+    public void aumentarCanal(){
+        canal++;
+    }
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void escolherCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+}
