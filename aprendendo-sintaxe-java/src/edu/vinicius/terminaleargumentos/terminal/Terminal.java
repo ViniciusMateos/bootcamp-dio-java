1a0f39b7e0f04ffc8866575896e55aa0e558df35
Vinicius Mateos <vinimateos.s@gmail.com>
2024-06-26 21:22:44 -0300

    feat: aulas de JavaDoc e Terminal e Argumentos

---
+++ aprendendo-sintaxe-java/src/edu/vinicius/terminaleargumentos/terminal/Terminal.java
@@ -0,0 +1,17 @@
+package edu.vinicius.terminaleargumentos.terminal;
+
+public class Terminal {
+
+    /*
+     * Vamos ilustrar como executar uma classe, depois de compilada, sem precisar usar a IDE.
+     * 
+     * - Abra o MS-DOS ou Power Shell;
+     * - Localize o diretório do seu projeto: cd 
+     * - Acesse a pasta **** bin: ** cd bin**;
+     * - Agora digite o comando:**java MinhaClasse ** (nome da sua classe sem a extensão .class).
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Olá, fui executado pelo temrinal.");        
+    }
+}
