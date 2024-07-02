4d91643efc8b9cc3154407ee2366b3184aaf248b
Vinicius Mateos <vinimateos.s@gmail.com>
2024-07-02 17:20:39 -0300

    feat: aula de condicionais

---
+++ aprendendo-sintaxe-java/src/edu/vinicius/condicionais/composta/CondicionalComposta.java
@@ -0,0 +1,19 @@
+package edu.vinicius.condicionais.composta;
+
+public class CondicionalComposta {
+    /*
+     * Algumas vezes, o nosso programa deverá seguir mais de uma jornada de execução, condionado a uma regra de negócio, 
+     * este cenário é demoninado Estrutura Condicional Composta.
+     */
+    public static void main(String[] args) {
+
+        int nota = 6;
+        
+        if(nota >= 7)
+         System.out.println("Aprovado"); // 1° jornada
+ 
+        else
+         System.out.println("Reprovado"); // 2° jornada
+     }
+
+} 
---
+++ aprendendo-sintaxe-java/src/edu/vinicius/condicionais/switchcase/PlanoOperadora.java
@@ -0,0 +1,47 @@
+package edu.vinicius.condicionais.switchcase;
+
+public class PlanoOperadora {
+
+    /*
+     * BASIC: 100 minutos de ligação;
+     * MÍDIA: 100 minutos de ligação + WhatsApp e Instagram grátis;
+     * TURBO: 100 minutos de ligação + WhatsApp e Instagram grátis + 5 GB Youtube.
+     */
+    
+    public static void main(String[] args) {
+
+        // Modo condicional convencional
+		String plano = "M"; //M / T
+		
+		if(plano == "B") {
+			System.out.println("100 minutos de ligação");
+		}else if(plano == "M") {
+			System.out.println("100 minutos de ligação");
+			System.out.println("WhatsApp e Instagram grátis");	
+		}else if(plano == "T") {
+			System.out.println("100 minutos de ligação");
+			System.out.println("WhatsApp e Instagram grátis");	
+			System.out.println("5Gb Youtube");	
+		}
+			
+        // Modo condicional switch/case
+        String plano2 = "M"; // M / T
+
+		switch (plano2) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+			}
+			case "M": {
+				System.out.println("WhatsApp e Instagram grátis");
+			}
+			case "B": {
+				System.out.println("100 minutos de ligação");
+			}
+		}
+		
+        /*
+         * Nesse caso sem usar o break, faz com que ele já adicione as vantagens dos planos a partir do outro.
+         * Fazendo com que escreva menos código e sendo mais eficiente do que if/else.
+         */
+	}
+}
