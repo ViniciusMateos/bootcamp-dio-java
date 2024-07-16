ee4b263b89ef6c96f71725d7f3fa52e0dd82af8b
Vinicius Mateos <vinimateos.s@gmail.com>
2024-07-15 23:03:48 -0300

    feat: introdução de aula de poo

---
+++ aprendendo-sintaxe-java/src/edu/vinicius/poo/classe/Escola.java
@@ -0,0 +1,28 @@
+package edu.vinicius.poo.classe;
+
+public class Escola {
+
+    // Criando objetos a partir da classe Estudante
+
+    /* 
+    public static void main(String[] args) throws Exception {
+        Estudante Estudante1 = new Estudante();
+        Estudante1.name= "John";
+        Estudante1.age= 12;
+        Estudante1.color= Color.FAIR;
+        Estudante1.sex= Sex.MALE;
+  
+        Estudante Estudante2 = new Estudante();
+        Estudante2.name= "Sophia";
+        Estudante2.age= 10;
+        Estudante2.color= Color.FAIR;
+        Estudante2.sex= Sex.FEMALE;
+  
+        Estudante Estudante3 = new Estudante();
+        Estudante3.name= "Lily";
+        Estudante3.age= 11;
+        Estudante3.color= Color.DARK;
+        Estudante3.sex= Sex.FEMALE;
+      }
+        */
+}
---
+++ aprendendo-sintaxe-java/src/edu/vinicius/poo/classe/Estudante.java
@@ -0,0 +1,23 @@
+package edu.vinicius.poo.classe;
+
+public class Estudante {
+    // Criando a classe Student
+// Com todas as características e compartamentos aplicados
+
+/*
+    String name;
+    int age;
+    Color color;
+    Sex sex;
+
+    void eating(Lunch lunch){
+      //NOSSO CÓDIGO AQUI
+    }
+    void drinking(Juice juice){
+      //NOSSO CÓDIGO AQUI
+    }
+    void running(){
+      //NOSSO CÓDIGO AQUI
+    }
+       */
+}
